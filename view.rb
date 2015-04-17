97008a2b6cfcfa2e4b0f3e2571ffc3f8a02ff3d8
Doraly Pantaleon <doralypantaleon@gmail.com>
2015-04-17 15:10:06 -0400

    Added 5 methods

--- view.rb
+++ view.rb
@@ -2,24 +2,46 @@ class View
 
   def welcome
     #{deck.topic}
-    puts '*' * 25
+    puts '*' * 100
     puts "Welcome to Ruby Flash Cards. To play, just enter the correct term for each definition.  Ready?  Go!"
-    puts '*' * 25
+    puts '*' * 100
   end
 
   def ask_question(card)
-    #Question: card.question
+    puts "Question: #{card.question}"
   end
 
   def result
-    #correct or incorrect
+    if card.answer == user_input ?  "Correct!" : "Incorrect, try again."
   end
 
   def goodbye
     #summary
   end
 
+  def user_input
+    gets.chomp
+  end
+
 end
 
 view = View.new
 view.welcome
+
+
+if ARGV != []
+  file = ARGV[0]
+else
+  file = 'flashcard_samples.txt'
+end
+
+puts "Welcome! You are using #{file}"
+puts "To play, just enter the correct term for each definition.  Ready?  Go!"
+begin
+  test = Deck.new(file)
+  test.study_and_review
+rescue Errno::ENOENT
+  puts "That wasn't a valid file. Please try again."
+end
+
+
