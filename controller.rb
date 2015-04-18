1ce8ff0835c51984c2b2d0ed945135444d685698
audibleblink <virtualsnyper@gmail.com>
2015-04-18 18:41:45 -0400

    add comments

--- controller.rb
+++ controller.rb
@@ -3,11 +3,7 @@ require_relative 'deck_model'
 require_relative 'parser'
 require_relative 'view'
 
-if ARGV != []
-  file = ARGV[0]
-else
-  file = 'flashcard_samples.txt'
-end
+file = ARGV.empty? ? 'flashcard_samples.txt' : ARGV[0] 
 
 def instantiate_deck(file)
   card_objects = Parser.parse_data(file).map {|hash| Card.new(hash)}
@@ -21,34 +17,33 @@ end
 
 def play(deck)
   View.welcome
-  begin
-    deck.cards.shuffle.each do |card|
-      count = 0
-      checker = nil
-      until count == 3 || checker == true
-        View.ask_question(card)
-        input = View.user_input
-        exit_program if input.upcase == "Q"
-        View.result(card, input)
-        count +=1
-        card.tries += 1
-        checker = card.answer.upcase == input
-      end
+  deck.cards.shuffle.each do |card|
+    count = 0
+    checker = nil
+    until count == 3 || checker
+      View.ask_question(card)
+      input = View.user_input
+      exit_program if input.upcase == "Q"
+      View.result(card, input)
+      count +=1
+      card.tries += 1
+      checker = card.answer.upcase == input
     end
-    View.goodbye
-    input = View.user_input
-    if input == "Q"
-      exit_program
-    elsif input == "N"
-      View.prompt_file_name
-      file = View.user_input
-      play(instantiate_deck(file))
-    else
-      "That wasn't a valid file. Please try again."
-    end
-  rescue Errno::ENOENT
-    puts "That wasn't a valid file. Please try again."
+  end
+  View.goodbye
+  input = View.user_input
+  if input == "Q"
+    exit_program
+  elsif input == "N"
+    View.prompt_file_name
+    file = View.user_input
+    play(instantiate_deck(file))
+  else
+    "That wasn't a valid file. Please try again."
   end
 end
 
 play instantiate_deck(file) #kickoff
+# defensive programming is good (raising exceptions) but be specific
+# the best place for it is also usually around where the user interfaces
+# with your program, assume your users are dumb
